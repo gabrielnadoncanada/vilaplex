360d204d229323a67add116ec9481d3c56aa8983
Gabriel Nadon <gabrielnadoncanada@gmail.com>
2024-04-26 21:23:32 -0500

    fix mobile version

--- resources/views/pages/templates/about.blade.php
+++ resources/views/pages/templates/about.blade.php
@@ -1,29 +1,32 @@
 <div class="mry-about mry-p-100-100">
-    <div class="container">
-        <div class="row justify-content-center">
-            <x-builder :blocks="$record->content"></x-builder>
-            <div class="col-lg-8">
+    <div class="container-fluid">
+        <div class="container">
+            <div class="row justify-content-center">
+                <x-builder :blocks="$record->content"></x-builder>
+                <div class="col-lg-8">
 
-                <div class="mry-numbering mry-fo">
-                    <div class="mry-border-text">03</div>
-                    <div class="mry-subtitle">Services</div>
-                </div>
+                    <div class="mry-numbering mry-fo">
+                        <div class="mry-border-text">03</div>
+                        <div class="mry-subtitle">Services</div>
+                    </div>
 
-                <div class="row justify-content-center">
-                    @foreach (App\Models\Service::published()->get() as $index => $service)
-                        <div class="col-lg-6">
-                            <div class="mry-text-center mry-mb-100">
-                                <h4 class="mry-mb-20 mry-fo mry-h4">{{$service->title}}</h4>
-                                <p class="mry-text mry-mb-20 mry-fo">{{$service->excerpt}}</p>
-                                <div class="mry-fo">
-                                    <a href="{{LaravelLocalization::getURLFromRouteNameTranslated( App::currentLocale(), 'routes.service', array('record' => $service->slug))}}"
-                                       class="mry-link mry-default-link">En savoir plus</a></div>
+                    <div class="row justify-content-center">
+                        @foreach (App\Models\Service::published()->get() as $index => $service)
+                            <div class="col-lg-6">
+                                <div class="mry-text-center mry-mb-100">
+                                    <h4 class="mry-mb-20 mry-fo mry-h4">{{$service->title}}</h4>
+                                    <p class="mry-text mry-mb-20 mry-fo">{{$service->excerpt}}</p>
+                                    <div class="mry-fo">
+                                        <a href="{{LaravelLocalization::getURLFromRouteNameTranslated( App::currentLocale(), 'routes.service', array('record' => $service->slug))}}"
+                                           class="mry-link mry-default-link">En savoir plus</a></div>
+                                </div>
                             </div>
-                        </div>
-                    @endforeach
+                        @endforeach
+                    </div>
                 </div>
             </div>
         </div>
+
     </div>
 </div>
 <x-sections.call-to-action/>
