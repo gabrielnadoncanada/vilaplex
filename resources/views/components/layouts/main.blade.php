5dfc103aa40e033273a91e88a847eb6dc5d2489c
Gabriel Nadon <gabrielnadoncanada@gmail.com>
2024-07-03 21:11:04 -0400

    fix

--- resources/views/components/layouts/main.blade.php
+++ resources/views/components/layouts/main.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="overflow-x-hidden h-full">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="is-animating overflow-x-hidden h-full">
 <head>
     <meta charset="utf-8">
     <meta name="application-name" content="{{ config('app.name') }}">
