552a46e52f5868676c7f41d087fd52d1441348f7
Gabriel Nadon <gabrielnadoncanada@gmail.com>
2024-09-05 18:16:42 -0400

    fix

--- resources/views/components/blocks/partials/column-content.blade.php
+++ resources/views/components/blocks/partials/column-content.blade.php
@@ -5,21 +5,18 @@
 
     <x-blocks.fields.image class="mb-8 lg:mb-10 aspect-square rounded-[10px] object-cover" :image="$image" />
 
- <div class="flex flex-col gap-y-4">
-     <x-text :as="$subtitle_level" theme="subtitle.center">
-         {{$subtitle_text}}
-     </x-text>
-     <x-text
-         :as="$heading_level"
-         :theme="$heading_size">
-         {{$heading_text}}
-     </x-text>
+  <x-text :as="$subtitle_level" theme="subtitle.center">
+    {{$subtitle_text}}
+  </x-text>
+    <x-text
+        :as="$heading_level"
+        :theme="$heading_size">
+        {{$heading_text}}
+    </x-text>
 
-     <x-text as="div" class="mx-auto mb-[10px] max-w-[650px]">
-         {!! $text !!}
-     </x-text>
- </div>
-
+  <x-text as="div" class="mx-auto mt-4 mb-[10px] max-w-[650px]">
+    {!! $text !!}
+  </x-text>
 
 
   <x-blocks.fields.buttons :buttons="$buttons" />
